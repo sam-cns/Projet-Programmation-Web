dd5aab7e08cc386255836830d4cf8fc844aa4c08
sam-cns <cohen-solal@et.esiea.fr>
2016-09-13 18:11:27 +0200

    Add files via upload
    Route pour ouvrir le fichier test

---
+++ routes.php
@@ -0,0 +1,30 @@
+<?php
+// Routes
+
+
+
+$app->get('/install', function ($request, $response, $args) {
+    // Sample log message
+    //$this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'install.phtml', $args);
+});
+
+$app->get('/test', function ($request, $response, $args) {
+    // Sample log message
+    //$this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'test.phtml', $args);
+});
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.html', $args);
+});
+
+
