6fb9e99057f7d0d4e87087e1468d5908908cc155
sam-cns <cohen-solal@et.esiea.fr>
2016-09-27 15:41:24 +0200

    Fichier route.php

--- routes.php
+++ routes.php
@@ -1,15 +1,20 @@
 <?php
 // Routes
-
-
-
+    
 $app->get('/install', function ($request, $response, $args) {
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'install.phtml', $args);
+$this->db;
+
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule::schema()->create('articles', function (\Illuminate\Database\Schema\Blueprint $table) {
+       $table->increments('id');
+        $table->string('name');
+        // Include created_at and updated_at
+       $table->timestamps();
+   });
+   return $this->renderer->render($response, 'install.phtml', $args);
 });
+
 
 $app->get('/test', function ($request, $response, $args) {
     // Sample log message
@@ -24,7 +29,11 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.html', $args);
+    return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+
+
+
 
 
